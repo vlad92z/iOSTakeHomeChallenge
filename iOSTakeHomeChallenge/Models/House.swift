9483e7763ff9d2079b446b04c65020fcafdfb051
Vlad <vlad92z@users.noreply.github.com>
2021-06-14 23:11:56 +0100

    Update House ViewController to support pagination and search. Add Unit Tests.

---
+++ iOSTakeHomeChallenge/Models/House.swift
@@ -0,0 +1,8 @@
+//
+//  House.swift
+//  iOSTakeHomeChallenge
+//
+//  Created by Vlad on 14/06/2021.
+//
+
+import Foundation
---
+++ iOSTakeHomeChallenge/Views/HouseTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  HouseTableViewCell.swift
+//  iOSTakeHomeChallenge
+//
+//  Created by Vlad on 14/06/2021.
+//
+
+import Foundation
